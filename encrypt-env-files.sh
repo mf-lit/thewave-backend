57b5f05e696244fe7fdd046bfe5e5c9dd93510af
marc <marc@vq5.net>
2026-07-20 09:40:48 +0100

    init repo

---
+++ encrypt-env-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Encrypt all .env files so they can be safely committed to git
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+for i in .env *.env copyparty/server.conf ; do
+	echo "Encrypting $i..."
+	sleep 1
+	gpg -c --armor $i
+	f=$(basename $i)
+	sed -i "1s/^/# Decrypt with \`gpg -d $f.asc\`\n/" $i.asc
+done
+
+
